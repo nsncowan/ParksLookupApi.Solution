1ddf000192829b6c8440d2b28250f2fd30955beb
Noah Cowan <nsncowan@gmail.com>
2023-04-20 14:50:15 -0700

    add model and db context

---
+++ ParksLookupApi/Models/Park.cs
@@ -0,0 +1,11 @@
+
+
+namespace ParksLookupApi.Models
+{
+  public class Park
+  {
+    public int ParkId { get; set; }
+    public string Name { get; set; }
+    public string State { get; set; }
+  }
+}
---
+++ ParksLookupApi/Models/ParksLookupApiContext.cs
@@ -0,0 +1,26 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ParksLookupApi.Models
+{
+  public class ParksLookupApiContext : DbContext
+  {
+    public DbSet<Park> Parks { get; set; }
+    // public DbSet<Review> Review { get; set; }
+
+    public ParksLookupApiContext(DbContextOptions<ParksLookupApiContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Destination>()
+        .HasData(
+          new Park { ParkId = 1, Name = "Olympic", State = "Washington"},
+          new Park { ParkId = 2, Name = "Denali", State = "Alaska"},
+          new Park { ParkId = 3, Name = "Everglades", State = "Florida"},
+          new Park { ParkId = 4, Name = "Grand Canyon", State = "Arizona"},
+          new Park { ParkId = 5, Name = "Yosemite", State = "California"}
+        );
+    }
+  }
+}
