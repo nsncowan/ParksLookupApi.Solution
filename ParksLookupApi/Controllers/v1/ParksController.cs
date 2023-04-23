5e88126f06e47725374a22fee4acbc4503eb3713
Noah Cowan <nsncowan@gmail.com>
2023-04-22 19:44:37 -0700

    add version folders,  version tags to controllers

--- ParksLookupApi/Controllers/v1/ParksController.cs
+++ ParksLookupApi/Controllers/v1/ParksController.cs
@@ -4,8 +4,10 @@ using ParksLookupApi.Models;
 
 namespace ParksLookupApi.Controllers
 {
-  [Route("api/[controller]")]
   [ApiController]
+  [Route("api/v{version:apiVersion}/[controller]")]
+  [ApiVersion("1.0")]
+  [ApiVersion("2.0")]
   public class ParksController : ControllerBase
   {
 // ====================================================
