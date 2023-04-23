c9f6bc4cc8487a841e8c61b84065d872db5aa647
Noah Cowan <nsncowan@gmail.com>
2023-04-22 18:10:48 -0700

    add builder services for api versioning

--- ParksLookupApi/Program.cs
+++ ParksLookupApi/Program.cs
@@ -2,6 +2,9 @@ using ParksLookupApi.Models;
 using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
 using Microsoft.Extensions.DependencyInjection;
+// ===========================THIS IS THE USING DIRECTIVE FOR VERSIONING============================================
+using Microsoft.AspNetCore.Mvc.Versioning;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,7 +28,17 @@ builder.Services.AddDbContext<ParksLookupApiContext>(
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+// ============================THIS IS THE VERSIONING SERVICES CODE ===========================================
+builder.Services.AddApiVersioning(opt =>
+                                    {
+                                        opt.DefaultApiVersion = new Microsoft.AspNetCore.Mvc.ApiVersion(1,0);
+                                        opt.AssumeDefaultVersionWhenUnspecified = true; // very useful: assumes a default version when client does not specify a version. This will prevent an unspecified version from breaking a client's app.
+                                        opt.ReportApiVersions = true;
+                                        opt.ApiVersionReader = ApiVersionReader.Combine(new UrlSegmentApiVersionReader(),
+                                                                                        new HeaderApiVersionReader("x-api-version"),
+                                                                                        new MediaTypeApiVersionReader("x-api-version"));
+                                    });
+// =======================================================================
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
